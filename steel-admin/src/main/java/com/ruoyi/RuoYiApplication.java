d2956e9b571e4e4232226a4db67d5bdd213dcc60
steelshadow39 <571424044@qq.com>
2023-06-12 14:59:12 +0800

    修改部分接口

--- steel-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ steel-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -12,8 +12,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class RuoYiApplication
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
+    }
 }
